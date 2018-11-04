673d6bfb05a3dd717bc0a9769aa5f8ba987b16f2
Martin Papenberg <martin.papenberg@hhu.de>
2018-11-04 12:01:59 +0100

    constraint matrix is sparse; removed some unnecessary constraints

--- R/ILP.R
+++ R/ILP.R
@@ -17,8 +17,6 @@
 #'  is needed for the commercial solvers. One of the interface packages
 #'  must be installed.
 #'
-#' @importFrom Matrix Matrix
-#'
 #' @export
 #'
 #' @references
@@ -42,47 +40,26 @@ item_assign_ilp <- function(distances, p, solver = "glpk") {
   n_items <- nrow(as.matrix(distances))
   group_size = n_items / p
   costs <- vectorize_weights(distances)
-  n_vars <- nrow(costs) + n_items # number of decision variables
 
   ## Specify the number of constraints:
-  n_tris <- choose(n_items, 3) * 3 # tri-angular constraints
-  n_c5 <- 1 # (constraint 5 in Bulhoes et al. 2017 ("Branch-and-price"))
-  n_c6 <- nrow(costs) # numbering continues: 6, 7, ...
-  n_c7 <- n_items - 1
-  n_c8 <- 1
-  n_c9 <- n_items ## constraint 9 not part of Bulhoes et al.'s formulation
-  n_constraints <- n_tris + n_c5 + n_c6 + n_c7 + n_c8 + n_c9
+  n_tris <- choose(n_items, 3) * 3 # triangular constraints
+  n_c9 <- n_items # number of group constraints
 
   ## Construct ILP constraint matrix
-  constraints <- matrix(0, ncol = n_vars, nrow = n_constraints)
-  colnames(constraints) <- c(costs$pair, paste0("y", 1:n_items))
-  rownames(constraints) <- c(paste0("tc", 1:n_tris), "c5",
-                               paste0("c6_", 1:n_c6),
-                               paste0("c7_", 1:n_c7),
-                               "c8", paste0("c9_", 1:n_c9))
-  constraints <- triangular_constraints(constraints, n_items)
-  constraints <- group_contraints(constraints, n_items, group_size)
-  constraints <- Matrix::Matrix(constraints, sparse = TRUE) ## TODO: init as sparse matrix
-
+  constraints <- sparse_constraints(n_items, costs$pair)
+  colnames(constraints) <- costs$pair
 
   ## Directions of the constraints:
   equalities <- c(rep(equality_signs$l, n_tris),
-                  rep(equality_signs$e, n_c5),
-                  rep(equality_signs$l, n_c6),
-                  rep(equality_signs$g, n_c7),
-                  rep(equality_signs$e, n_c8),
                   rep(equality_signs$e, n_c9))
 
   # Right-hand-side of ILP
-  rhs <- c(rep(1, nrow(constraints) - 1 - n_c9),
-           p, rep(group_size - 1, n_c9)) #  p = number of clusters
+  rhs <- c(rep(1, n_tris), rep(group_size - 1, n_c9)) #  p = number of clusters
 
   # Objective function of the ILP
-  obj_function <- c(costs$costs, rep(0, n_items))
+  obj_function <- costs$costs
 
   ## Give names to all objects for inspection purposes
-  names(rhs) <- rownames(constraints)
-  names(equalities) <- rownames(constraints)
   names(obj_function) <- colnames(constraints)
 
   ## return instance
@@ -150,62 +127,84 @@ vectorize_weights <- function(distances) {
   return(costs)
 }
 
-# Indices for construction of sparse constraint matrix
+#' Construct indices for a sparse matrix representation of triangular constraints
+#' @param n_items How many items are there
+#' @param pair_names A character vector of names representing the item pairs.
+#'   Must have the form that is contained in the ILP data.frame costs$pair.
+#' @return A sparse matrix representing the left-hand side of the ILP (A in Ax ~ b)
+#'
+#' @importFrom Matrix sparseMatrix
+sparse_constraints <- function(n_items, pair_names) {
+  tri <- vectorized_triangular(n_items, pair_names)
+  gr  <- vectorized_group(n_items, pair_names)
+  return(Matrix::sparseMatrix(c(tri$i, gr$i), c(tri$j, gr$j), x = c(tri$x, gr$x)))
+}
+
+#' Construct indices for a sparse matrix representation of triangular constraints
+#' @param n_items How many items are there
+#' @param pair_names A character vector of names representing the item pairs.
+#'   Must have the form that is contained in the ILP data.frame costs$pair.
+#' @return A list of indices to be used as input parameters of Matrix::sparseMatrix
+#'
 vectorized_triangular <- function(n_items, pair_names) {
-  triangular_constraints <- choose(n_items, 3) * 3
+  triangular_constraints <- choose(n_items, 3)
   coef_per_constraint <- 3
   # number of coefficients in constraint matrix:
-  triangular_coefficients <- triangular_constraints * coef_per_constraint
-  group_coefficients <- 3 * n_items
-  row_indices <- vector(length = triangular_coefficients)
-  col_indices <- row_indices
-  xes         <- row_indices
-  row_indices[1:triangular_coefficients] <- rep(1:(3*n_items), each = 3)
-  ## (1) Triangular constraints - columns and coefficients
+  col_indices <- matrix(ncol = triangular_constraints, nrow = coef_per_constraint * 3)
+  row_indices <- rep(1:(triangular_constraints*3), each = 3)
+  xes <- rep(c(-1, 1, 1, 1, -1, 1, 1, 1, -1), triangular_constraints)
+  ## Fill columns for constraints
   counter <- 1
   for (i in 1:n_items) {
     for (j in 2:n_items) {
       for (k in 3:n_items) {
         ## ensure that only legal constraints are inserted:
         if (!(i < j) | !(j < k)) next
-        ## Offset for addressing the index vectors
-        offset <- (counter - 1) * coef_per_constraint * 3
         ## Construct indices
-        col_indices[offset + c(1, 4, 7)] <- which(paste0("x", i, "_", j) == pair_names)
-        col_indices[offset + c(2, 5, 8)] <- which(paste0("x", i, "_", k) == pair_names)
-        col_indices[offset + c(3, 6, 9)] <- which(paste0("x", j, "_", k) == pair_names)
-        ## Values of the triangular coefficients
-        xes[offset + c(1, 4, 7)] <- c(-1, 1, 1)
-        xes[offset + c(2, 5, 8)] <- c(1, -1, 1)
-        xes[offset + c(3, 6, 9)] <- c(1, 1, -1)
+        pairs <- c(paste0("x", i, "_", j), paste0("x", i, "_", k), paste0("x", j, "_", k))
+        indices <- match(pairs, pair_names)
+        col_indices[, counter] <- rep(indices, 3)
         counter <- counter + 1
       }
     }
   }
-  ## (2) Group constraints (this is still buggy; rework)
-  counter <- 0
-  for (i in 1:n_items) { ## i: current item that may be a cluster leader
-    for (j in 1:n_items) {
-      if (i < j) {
-        row_indices[triangular_coefficients + counter] <- triangular_constraints + counter
-        col_indices[triangular_coefficients + counter] <- which(paste0("x", i, "_", j) == pair_names)
-        xes[triangular_coefficients + counter]         <- 1
-        counter <- counter + 1
-      }
-      if (j < i) {# i may be lower or higher index!)
-        row_indices[triangular_coefficients + counter] <- triangular_constraints + counter
-        col_indices[triangular_coefficients + counter] <- which(paste0("x", j, "_", i) == pair_names)
-        xes[triangular_coefficients + counter]         <- 1
-        counter <- counter + 1
-      }
-    }
-  }
-  return(list(i = row_indices, j = col_indices, x = xes))
+  return(list(i = row_indices, j = c(col_indices), x = xes))
 }
 
-# for creating all indices for group size constraint; to replace the inner for-loop
-# in group size constraints above
+#' Construct indices for a sparse matrix representation of group constraints
+#' @param n_items How many items does the instance have
+#' @param pair_names A character vector of names representing the item pairs.
+#'   Must have the form that is contained in the ILP data.frame costs$pair.
+#' @return A list of indices to be used as input parameters of Matrix::sparseMatrix
+vectorized_group <- function(n_items, pair_names) {
+  coef_per_constraint <- (n_items - 1)
+  group_coefficients <- coef_per_constraint * n_items
+  row_indices <- rep((1:n_items) + (3 * choose(n_items, 3)), each = coef_per_constraint)
+  col_indices <- matrix(ncol = n_items, nrow = coef_per_constraint)
+  xes         <- rep(1, length = group_coefficients)
+
+  for (i in 1:n_items) {
+    connections <- all_connections(i, n_items)
+    pairs <- connections_to_pair(connections)
+    col_indices[, i] <- match(pairs, pair_names)
+  }
+  return(list(i = row_indices, j = c(col_indices), x = xes))
+}
+
+#' Get character representation of item connections
+#' @param connections a data.frame returned by `all_connections`
+connections_to_pair <- function(connections) {
+  apply(connections, 1, function(x) paste0("x", paste0(x, collapse = "_")))
+}
+
+#' Find all connections of an element
+#' @param i The point for which the connections are sought (must be in 1...n)
+#' @param n The number of points
+#' @return A `data.frame` with two columns representing start and end point.
+#'   The second column always contains the "larger number" point.
 all_connections <- function(i, n) {
+  if (i > n | i <= 0)
+    stop("Error in function `all_connections`: cannot find connections for element that is outside of legal range")
   connections <- expand.grid(i, setdiff(1:n, i))
   # put lower index in first column
   wrongorder <- connections[, 1] > connections[, 2]
@@ -213,77 +212,4 @@ all_connections <- function(i, n) {
   connections[, 2][wrongorder] <- connections[, 1][wrongorder]
   connections[, 1][wrongorder] <- temp
   return(connections)
-}
-
-# Insert the coefficients of the triangular constraints into the constraint matrix
-triangular_constraints <- function(constraints, n_items) {
-  ## (1) Triangular constraints
-  counter <- 1
-  for (i in 1:n_items) {
-    for (j in 2:n_items) {
-      for (k in 3:n_items) {
-        ## ensure that only legal constraints are inserted:
-        if (!(i < j) | !(j < k)) next
-        ## offset for addressing the data.frame:
-        offset <- (counter - 1) * 3
-        ## triangular constraint 1
-        constraints[offset + 1, paste0("x", i, "_", j)] <- -1
-        constraints[offset + 1, paste0("x", i, "_", k)] <- 1
-        constraints[offset + 1, paste0("x", j, "_", k)] <- 1
-        constraints[offset + 1, paste0("y", k)] <- 1
-        ## triangular constraint 2
-        constraints[offset + 2, paste0("x", i, "_", j)] <- 1
-        constraints[offset + 2, paste0("x", i, "_", k)] <- -1
-        constraints[offset + 2, paste0("x", j, "_", k)] <- 1
-        ## triangular constraint 3
-        constraints[offset + 3, paste0("x", i, "_", j)] <- 1
-        constraints[offset + 3, paste0("x", i, "_", k)] <- 1
-        constraints[offset + 3, paste0("x", j, "_", k)] <- -1
-        ## increase counter
-        counter <- counter + 1
-      }
-    }
-  }
-  return(constraints)
-}
-
-## TODO next: vectorize group constraints (maybe only use group size constraint for a first test)
-## Inserts constraints specifying cluster number and cluster size
-group_contraints <- function(constraints, n_items, group_size) {
-  ## The numbers are taken from Bulhoes et al. 2017 ("Branch-and-price")
-  ## Constraint (5): make first element leader of cluster
-  constraints["c5", "y1"] <- 1
-  ## Constraint (6): restrictions on cluster leadership
-  counter <- 1
-  for (i in 1:n_items) {
-    for (j in 2:n_items) {
-      if (i >= j) next
-      constraints[paste0("c6_", counter), paste0("x", i, "_", j)] <- 1
-      constraints[paste0("c6_", counter), paste0("y", j)] <- 1
-      counter <- counter + 1
-    }
-  }
-  ## Constraint (7): more restrictions on cluster leadership
-  counter <- 1
-  for (j in 2:n_items) {
-    constraints[paste0("c7_", counter), paste0("y", j)] <- 1
-    for (i in 1:n_items) {
-      if (i >= j) next
-      constraints[paste0("c7_", counter), paste0("x", i, "_", j)] <- 1
-    }
-    counter <- counter + 1
-  }
-  ## Constraint (8): Specify number of clusters
-  constraints["c8", paste0("y", 1:n_items)] <- 1
-  ## Constraint (9) enforces the clusters to be of equal size (I call
-  ## it constraint (9) but it is not taken from Bulhoes 2017)
-  for (i in 1:n_items) { ## i: current item that may be a cluster leader
-    for (j in 1:n_items) {
-      if (i < j)
-        constraints[paste0("c9_", i), paste0("x", i, "_", j)] <- 1
-      if (j < i) # i may be lower or higher index!)
-        constraints[paste0("c9_", i), paste0("x", j, "_", i)] <- 1
-    }
-  }
-  return(constraints)
 }
