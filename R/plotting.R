fc18d078d1561c2d4a0bd95ede64c091e08101c2
Martin Papenberg <martin.papenberg@hhu.de>
2019-02-05 11:47:50 +0100

    reworked pch argument (now also used to distinguish clusters)

--- R/plotting.R
+++ R/plotting.R
@@ -10,11 +10,15 @@
 #' @param ylab The label for the y-axis
 #' @param cols The coloring of the groups. Does not need to be passed.
 #'     If it is passed, it needs to be a character vector of the same
-#'     length as there are groups, and each element of the vector is a
+#'     length as there are clusters, and each element of the vector is a
 #'     color.
-#' @param pch The symbol used for data points, see ?par
+#' @param pch A numeric vector representing the symbol used to plot
+#'     data points (see \code{\link{par}}). Should either be of length 1,
+#'     then the same symbol is used for all plots, or should have the
+#'     same length as there are different clusters. In the latter case,
+#'     each cluster has a different plotting symbol.
 #' @param main The title of the plot
-#' @param cex The size of the plotting symbols, see ?par
+#' @param cex The size of the plotting symbols, see \code{\link{par}}
 #' @param cex.axis The size of the values on the axes
 #' @param cex.axis The size of the labels of the axes
 #'
@@ -23,13 +27,45 @@
 #' @importFrom grDevices rainbow
 #' @importFrom graphics lines par plot
 #'
+#' @details In most cases, the argument `clustering` is a vector
+#'   returned by one of the functions \code{\link{anticlustering}} or
+#'   \code{\link{clustering}}. However, the plotting function can also
+#'   be used to plot the results of other cluster functions such as
+#'   \code{\link{kmeans}}.
+#'
+#' @examples
+#'
+#' n_elements <- 90
+#' features <- matrix(runif(n_elements * 2), ncol = 2)
+#' n_groups <- 3
+#' clusters <- clustering(features, n_groups)
+#' anticlusters <- anticlustering(features, n_groups, method = "sampling")
+#' par(mfrow = c(1, 2))
+#' plot_clusters(features, clusters, pch = c(15, 16, 17))
+#' plot_clusters(features, anticlusters, pch = c(15, 16, 17))
+#'
 plot_clusters <- function(features, clustering, connect_clusters = FALSE,
-                          xlab = NULL, ylab = NULL, cols = NULL,
+                          xlab = NULL, ylab = NULL, col = NULL,
                           pch = 19, main = "", cex = 1.2,
                           cex.axis = 1.2, cex.lab = 1.2) {
-
   if (ncol(features) != 2)
     stop("Can only plot two features")
+  if (nrow(features) != length(clustering))
+    stop("There must be a cluster for each element - nrow(features) != length(clustering)")
+
+  ## Just in case anybody passes something weird (i.e., cluster numbers
+  ## do not start at 1):
+  clustering <- as.numeric(factor(clustering))
+
+  ## More input handling:
+  n_cliques <- length(unique(clustering))
+  if (length(pch) == n_cliques)
+    pch <- pch[clustering]
+  else if (length(pch) == 1)
+    pch <- pch # :-)
+  else
+    stop("Length of argument pch must be 1 the number of different anticlusters")
+
 
   ## Specify axis labels if they cannot be assumed from data matrix or argument
   if (is.null(xlab))
@@ -39,20 +75,19 @@ plot_clusters <- function(features, clustering, connect_clusters = FALSE,
 
   x <- features[, 1]
   y <- features[, 2]
-  n_cliques <- length(unique(clustering))
-  if (is.null(cols) == TRUE)
-    cols <- rainbow(n_cliques)
+  if (is.null(col) == TRUE)
+    col <- rainbow(n_cliques)
   ## Set colors for cliques
-  if (length(cols) != n_cliques)
+  if (length(col) != n_cliques)
     stop("If you specify colors, you have to specify as many colors as there are clusters")
-  cols <- cols[clustering]
+  col <- col[clustering]
   def_mar <- c(5, 4, 4, 2) + 0.1
   par(mar = def_mar + c(0, 1, 0, 0))
   plot(x, y, las = 1, cex.axis = cex.axis, cex.lab = cex.lab,
-       col = cols, xlab = xlab, ylab = ylab, cex = cex,
+       col = col, xlab = xlab, ylab = ylab, cex = cex,
        xaxt = "n", yaxt = "n", pch = pch, main = main)
   if (connect_clusters)
-    draw_all_cliques(x, y, clustering, cols = cols)
+    draw_all_cliques(x, y, clustering, cols = col)
   par(mar = def_mar)
 }
 
