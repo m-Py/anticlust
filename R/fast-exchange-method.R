fa43e654cbe61ba211d9ba10b55a2b5516e08b7f
Martin Papenberg <martin.papenberg@hhu.de>
2019-07-02 09:38:24 +0200

    computation of nearest neighbors now deals with NA. Elements with NA are potential neighbors among each other.

--- R/fast-exchange-method.R
+++ R/fast-exchange-method.R
@@ -58,24 +58,69 @@ fast_exchange_ <- function(data, clusters, categories, nearest_neighbors) {
 
 
 #' Get neigbours for fast preclustering (by category)
+#' @roRd
+#'
+#' @details
+#'
+#' Computes the k nearest neighbours for each input element using
+#' RANN::nn2. Deals with NA: elements with NA are treated as
+#' neighbours among each other because RANN::nn2 does not deal with
+#' NA. Computes neighbors within each category because exchange
+#' partners are only sought among neighbors and members of the same
+#' category (it would not make sense to compute nearest neighbors
+#' across categories because this function is only used as preprocessing
+#' for the exchange algorithm)
+#'
 
 get_neigbours <- function(features, k_neighbours, categories) {
-  ncategories <- length(unique(categories))
-  nns <- by(features, categories, RANN::nn2, k = k_neighbours + 1)
-  nns <- lapply(nns, function(x) x$nn.idx)
-  ## get original indices for each category
-  ## (converts indices per category in global indices)
-  get_index <- function(i, x) {
-    which(categories == i)[x[[i]]]
+
+  ## indicices of non NA values; RANN::nn2 does not deal with NA
+  bool_complete <- complete.cases(features)
+  complete <- which(bool_complete)
+  not_complete <- which(!bool_complete)
+  ## exclude NA elements from nearest neighbor computation
+  features <- features[complete, ]
+  if (argument_exists(categories)) {
+    categories <- categories[complete]
   }
-  idx <- lapply(1:ncategories, get_index, x = nns)
-  dims <- lapply(nns, function(x) dim(x))
-  ## change dimensions to original nearest neighbour structure
-  adjust_dims <- function(a, b) {
-    dim(a) <- dim(b)
-    a
+
+  ## Compute nearest neighbors; within categories if categories
+  ## are available!
+  k_neighbours <- min(nrow(features), k_neighbours + 1)
+  if (!argument_exists(categories)) {
+    idx <- RANN::nn2(features, k = k_neighbours)$nn.idx
+  } else {
+    ncategories <- length(unique(categories))
+    nns <- by(features, categories, RANN::nn2, k = k_neighbours)
+    nns <- lapply(nns, function(x) x$nn.idx)
+    ## get original indices for each category
+    ## (converts indices per category in global indices)
+    ## i is a category; x is the NN data structure
+    get_index <- function(i, x) {
+      which(categories == i)[x[[i]]]
+    }
+    idx <- lapply(1:ncategories, get_index, x = nns)
+    ## change dimensions to original nearest neighbour structure
+    adjust_dims <- function(a, b) {
+      dim(a) <- dim(b)
+      a
+    }
+    idx <- mapply(adjust_dims, idx, nns)
+    idx <- do.call(rbind, idx)
+    idx <- sort_by_col(idx, 1)
   }
-  idx <- mapply(adjust_dims, idx, nns)
-  idx <- do.call(rbind, idx)
+
+  ## now deal with NA:
+  if (sum(not_complete) >= 1) {
+    ## recover original indices (before NA exclusion)
+    dims <- dim(idx)
+    idx <- complete[idx]
+    dim(idx) <- dims ## recover original structure
+    ## make elements with NA exchange partners among each other:
+    NA_partners <- sample(not_complete, size = (k_neighbours - 1) * length(not_complete), replace = TRUE)
+    not_complete_idx <- cbind(not_complete, matrix(NA_partners, ncol = (k_neighbours - 1)))
+    idx <- rbind(idx, not_complete_idx)
+    colnames(idx) <- NULL
+  }
   sort_by_col(idx, 1)
 }
