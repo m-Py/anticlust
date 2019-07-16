ad2303b7810190667c99e9a099d92a1eaca4f1df
Martin Papenberg <martin.papenberg@hhu.de>
2019-07-16 17:53:40 +0200

    added functions for subset selection

---
+++ R/init-subset-selection.R
@@ -0,0 +1,77 @@
+
+#' Generates an initial cluster assignment for subset selection
+#'
+#' This function returns a vector that can be used as the K argument
+#' in the function \code{\link{anticlustering}}. Some entries in this
+#' vector may be NA enable subset selection procedures (i.e., not each
+#' item is assigned to a set).
+#'
+#' @param N The total number of items
+#' @param K The number of subsets to be generated
+#' @param n Either: a number indicating how many elements are selected
+#'     per subset. Or: A vector of length K. In the latter case, entries
+#'     represents the n per subset.
+#' @param groups A vector of length N, alternative argument to N and K.
+#'     Represents the group for an element.
+#' @return The initialized cluster for each element
+#'
+#' @examples
+#'
+#' # Example of how to use the function to create two subset of stimuli
+#' # from the schaper2019 data set. Two sets of n = 20 each are created,
+#' # one with kitchen items only and one with bathroom items only. The
+#' # sets are parallelized on two features.
+#'
+#' head(schaper2019)
+#' features <- schaper2019[, 3:6]
+#'
+#' groups <- anticlustering(
+#'   features,
+#'   K = subset_selection(groups = schaper2019$room, n = c(20, 20)),
+#'   categories = schaper2019$room,
+#'   objective = mean_sd_obj # this objective function makes most sense for subset selection
+#' )
+#'
+#' table(K, schaper2019$room)
+#' # Compare feature means by room
+#' by(features, groups, function(x) round(colMeans(x), 2))
+#' # Compare standard deviations by room
+#' by(features, groups, function(x) round(apply(x, 2, sd), 2))
+#'
+#' @export
+
+subset_selection <- function(N = NULL, K = NULL, n, groups = NULL) {
+  if (argument_exists(N) & argument_exists(groups)) {
+    stop("do not pass both `N` and `groups` argument")
+  }
+  if (argument_exists(K) & argument_exists(groups)) {
+    stop("do not pass both `K` and `groups` argument")
+  }
+  if (!argument_exists(K) & !argument_exists(groups)) {
+    if (length(n) == 1) {
+      stop("If only the argument `n` is passed, `n` must be of length > 1")
+    }
+    return(sample(rep(1:length(n), n)))
+  }
+
+  if (argument_exists(N)) {
+    groups <- rep_len(1:K, N)
+    N <- rep(N / K, K) # N per group
+  } else {
+    groups <- as.numeric(as.factor(groups))
+    N <- table(groups)
+    K <- length(N)
+  }
+  if (length(n) == 1) {
+    n <- rep(n, K)
+  }
+
+  slots <- rep(99, sum(N))
+  for (i in 1:K) {
+    ## how many NAs must be filled:
+    fill_NA <-  N[i] - n[i]
+    real_slots <- N[i] - fill_NA
+    slots[groups == i] <- sample(c(rep(i, real_slots), rep(NA, fill_NA)))
+  }
+  slots
+}
---
+++ R/mean_sd_objective-function.R
@@ -0,0 +1,30 @@
+
+#' An objective function measuring similarity via discrepancy features
+#'
+#' Uses means and standard deviations
+#'
+#' @param clusters A clustering vector
+#' @param features The features
+#'
+#' @return A value quantifying set similarity as the total absolute deviations
+#'     between sets on all features.
+#'
+#' @export
+#'
+
+mean_sd_obj <- function(clusters, features) {
+   features <- features[!is.na(clusters), ]
+   clusters <- clusters[!is.na(clusters)]
+   K <- length(unique(clusters))
+   O_min  <- featurewise_diff(by(features, clusters, colMeans), K)
+   SD_min <- featurewise_diff(by(features, clusters, function(x) apply(x, 2, sd)), K)
+   (O_min + SD_min) * (-1)
+}
+
+## Convert output from `by` to matrix and compute difference based on means / sds etc
+## for all features
+featurewise_diff <- function(x, K) {
+  mat <- matrix(unlist(x), ncol = K)
+  mat <- apply(mat, 1, diff)
+  sum(abs(mat))
+}
