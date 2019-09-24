ac2ad116cf5115bc6c4b5aadebca8a09a6a9afb7
Martin Papenberg <martin.papenberg@hhu.de>
2019-09-24 18:10:35 +0200

    adapted tests to change in clustering algorithm

--- tests/testthat/test-heuristics.R
+++ tests/testthat/test-heuristics.R
@@ -7,45 +7,12 @@ test_that("heuristic clustering works for different inputs", {
     m_features <- m
     ## vary number of anticlusters
     for (p in 2:5) {
-      p_anticlusters <- p
       n_elements <- p * 3 # n-elements must be multiplier of p
       features <- matrix(rnorm(n_elements * m_features), ncol = m_features)
-      clusters <- equal_sized_kmeans(features, p)
+      clusters <- centroid_clustering(features, N = n_elements, K = p)
       expect_equal(legal_number_of_clusters(features, clusters), NULL)
-    }
-  }
-})
-
-test_that("heuristic clustering produces expected output", {
-  ## Vary number of features
-  for (m in 1:4) {
-    m_features <- m
-    ## Vary number of clusters
-    for (p in 2:4) {
-      p_anticlusters <- p
-      n_elements <- p * 5 # n-elements must be multiplier of p
-      features <- matrix(rnorm(n_elements * m_features), ncol = m_features)
-      cluster_init <- kmeans(features, p_anticlusters)$cl
-      centers  <- cluster_centers(features, cluster_init)
-      clust_dist <- dist_from_centers(features, centers, squared = TRUE)
-      clusters <- heuristic_cluster_assignment(clust_dist)
-      ## Assert that each element is assigned only once
-      expect_equal(sum(duplicated(clusters)), 0)
-      ## Check dimensionality of output
-      expect_equal(ncol(clusters), p)
-      expect_equal(nrow(clusters), n_elements / p)
-
-      ## Test the cluster_to_long method (is the correct order preserved?)
-      long_clusters <- clusters_to_long(clusters)
-      expect_equal(legal_number_of_clusters(features, long_clusters), NULL)
-      for (i in 1:nrow(clusters)) {
-        for (j in 1:ncol(clusters)) {
-          index <- clusters[i, j]
-          ## The element at this index in long_cluster must correspond
-          ## to the column in matrix `cluster` (both encode the cluster)
-          expect_equal(long_clusters[index], j)
-        }
-      }
+      clusters <- centroid_clustering(features = NULL, distances = dist(features), N = n_elements, K = p)
+      expect_equal(legal_number_of_clusters(features, clusters), NULL)
     }
   }
 })
@@ -63,7 +30,7 @@ test_that("heuristic anticlustering produces expected output", {
     preclusters <- NULL
     if (conditions$preclustering[i] == TRUE) {
       n_preclusters <- n_elements / p_anticlusters
-      preclusters <- equal_sized_kmeans(features, n_preclusters)
+      preclusters <- centroid_clustering(features, N = n_elements, K = n_preclusters)
       ## Legal number of preclusters?
       expect_equal(legal_number_of_clusters(features, preclusters), NULL)
       ## Expected number of preclusters?
