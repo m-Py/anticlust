2813bfce988f7c7e8b94f6e97fa70097ee27e45d
Martin Papenberg <martin.papenberg@hhu.de>
2019-10-11 09:24:35 +0200

    ensure that correct number of preclusters is computed within categories

--- R/precluster_by_category.R
+++ R/precluster_by_category.R
@@ -2,11 +2,24 @@
 # Compute preclusters within categories
 precluster_per_category <- function(features, categories, K) {
   if (preclustering_possible(categories, K)) {
-    # save order an restore
-    data <- cbind(categories, 1:nrow(features), features)
+    N <- nrow(features)
+    unique_categories <- unique(categories)
+    n_categories <- length(unique_categories)
+    # save original order to restore before returning
+    data <- cbind(categories, 1:N, features)
     data <- sort_by_col(data, 1)
-    clusters_by_catgory <- by(data[, -c(1, 2)], data[, 1], balanced_clustering, K = K)
-    data$clusters <- unlist(clusters_by_catgory)
+    # precluster within each category:
+    cl <- list()
+    for (i in 1:n_categories) {
+      tmp_data <- data[data[, 1] == unique_categories[i], -c(1, 2)]
+      cl[[i]] <- balanced_clustering(tmp_data, K = nrow(tmp_data) / K)
+    }
+    # ensure that clusters in different categories have different cluster numbers
+    to_be_added <- sapply(cl, length) / K
+    to_be_added <- cumsum(to_be_added) - to_be_added[1]
+    cl <- lapply(seq_along(cl), function(i) cl[[i]] + to_be_added[i])
+    # return data sorted in original order
+    data$clusters <- unlist(cl)
     return(sort_by_col(data, 2)$clusters)
   }
   stop("Not today.")
