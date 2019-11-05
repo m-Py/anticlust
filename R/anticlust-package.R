d7cfbed9a4867710e6187fffdb481d2983ccd43e
Martin Papenberg <martin.papenberg@hhu.de>
2019-11-05 16:08:10 +0100

    built package using roxypackage

---
+++ R/anticlust-package.R
@@ -0,0 +1,21 @@
+#' \packageDescription{anticlust}
+#'
+#' The DESCRIPTION file:
+#' \tabular{ll}{
+#' Package: \tab anticlust\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.3.0-1\cr
+#' Date: \tab 2019-11-05\cr
+#' Depends: \tab R (>= 3.0.0)\cr
+#' Encoding: \tab UTF-8\cr
+#' License: \tab MIT + file LICENSE\cr
+#' URL: \tab https://github.com/m-Py/anticlust\cr
+#' }
+#'
+#' @title
+#' \packageTitle{anticlust}
+#' @author
+#' \packageAuthor{anticlust}
+#'
+#' Maintainer: \packageMaintainer{anticlust}
+"_PACKAGE"
