f0390f2242395e37c6caa3ba5f3680d855f8d9a3
Martin Papenberg <martin.papenberg@hhu.de>
2019-11-05 17:04:37 +0100

    this version needed

--- R/anticlust-package.R
+++ R/anticlust-package.R
@@ -6,7 +6,7 @@
 #' Type: \tab Package\cr
 #' Version: \tab 0.3.0-1\cr
 #' Date: \tab 2019-11-05\cr
-#' Depends: \tab R (>= 3.0.0)\cr
+#' Depends: \tab R (>= 3.4.0)\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab MIT + file LICENSE\cr
 #' URL: \tab https://github.com/m-Py/anticlust\cr
