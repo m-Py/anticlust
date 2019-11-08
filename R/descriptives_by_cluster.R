51e0fab5360b79d4dfdb6f42ab544d4a3a6a0116
Martin Papenberg <martin.papenberg@hhu.de>
2019-11-08 13:18:44 +0100

    added function that prints descriptive statistics by cluster

---
+++ R/descriptives_by_cluster.R
@@ -0,0 +1,43 @@
+
+#' Means and standard deviations by group variable formatted in table
+#' 
+#' @param features A data frame of features
+#' @param groups A grouping vector
+#' @param decimals The number of decimals
+#' 
+#' @return A table that illustrates means and standard deviations (in brackets)
+#' 
+#' @examples 
+#'
+#' data(iris)
+#' mean_sd_tab(iris[, -5], iris[, 5])
+#' 
+#' @author: Martin Papenberg
+#' @export
+
+mean_sd_tab <- function(features, groups, decimals = 2) {
+  K <- length(unique(groups))
+  # Means
+  means <- by(features, groups, colMeans)
+  means <- t(matrix(unlist(means), ncol = K))
+  mode(means) <- "numeric"
+  means <- rbind(means, apply(means, 2, range_diff))
+  means <- apply(means, 2, prmisc::force_decimals, decimals = decimals)
+  # SDS
+  sds <- by(features, groups, function(x) apply(x, 2, sd))
+  sds <- t(matrix(unlist(sds), ncol = K))
+  mode(sds) <- "numeric"
+  sds <- rbind(sds, apply(sds, 2, range_diff))
+  sds <- apply(sds, 2, prmisc::force_decimals, decimals = decimals)
+  # Merge Means and SDS
+  values <- paste0(paste(means, sds, sep = " ("), ")")
+  dim(values) <- c(K + 1, length(means) / (K + 1))
+  colnames(values) <- colnames(features)
+  values
+}
+
+# Determine the maximum range in a vector
+range_diff <- function(x) {
+  x <- round(x, 2) # so that it is consistent in the table
+  diff(range(x))
+}
