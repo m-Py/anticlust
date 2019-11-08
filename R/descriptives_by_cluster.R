ee2dc54bcf78d215f2c88870c26ef92f3ff0381c
Martin Papenberg <martin.papenberg@hhu.de>
2019-11-08 14:53:46 +0100

    added na.rm option to descriptive statistics table

--- R/descriptives_by_cluster.R
+++ R/descriptives_by_cluster.R
@@ -15,19 +15,19 @@
 #' @author: Martin Papenberg
 #' @export
 
-mean_sd_tab <- function(features, groups, decimals = 2) {
+mean_sd_tab <- function(features, groups, decimals = 2, na.rm  = FALSE) {
   K <- length(unique(groups))
   # Means
-  means <- by(features, groups, colMeans)
+  means <- by(features, groups, colMeans, na.rm = na.rm)
   means <- t(matrix(unlist(means), ncol = K))
   mode(means) <- "numeric"
-  means <- rbind(means, apply(means, 2, range_diff))
+  means <- rbind(means, apply(means, 2, range_diff, na.rm = na.rm))
   means <- apply(means, 2, prmisc::force_decimals, decimals = decimals)
   # SDS
-  sds <- by(features, groups, function(x) apply(x, 2, sd))
+  sds <- by(features, groups, function(x) apply(x, 2, sd, na.rm = na.rm))
   sds <- t(matrix(unlist(sds), ncol = K))
   mode(sds) <- "numeric"
-  sds <- rbind(sds, apply(sds, 2, range_diff))
+  sds <- rbind(sds, apply(sds, 2, range_diff, na.rm = na.rm))
   sds <- apply(sds, 2, prmisc::force_decimals, decimals = decimals)
   # Merge Means and SDS
   values <- paste0(paste(means, sds, sep = " ("), ")")
@@ -37,7 +37,7 @@ mean_sd_tab <- function(features, groups, decimals = 2) {
 }
 
 # Determine the maximum range in a vector
-range_diff <- function(x) {
+range_diff <- function(x, na.rm) {
   x <- round(x, 2) # so that it is consistent in the table
-  diff(range(x))
+  diff(range(x, na.rm = na.rm))
 }
