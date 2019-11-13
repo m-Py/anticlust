ece1690845ce2cce3e01d047f1a6173e7e21e4e1
Martin Papenberg <martin.papenberg@hhu.de>
2019-11-13 20:09:24 +0100

    trying something for divide and select

--- R/divide_select.R
+++ R/divide_select.R
@@ -66,7 +66,7 @@ select_stimuli <- function(
   n = NULL
 ) {
   if (argument_exists(split_by) && argument_exists(n)) {
-    groups <- divide_and_select(data, split_by, equalize, design, n)
+    groups <- subset_minmax_anticlustering(data, split_by, equalize, design, n)
   } else if (argument_exists(split_by) && !argument_exists(n)) {
     groups <- min_max_anticlustering(data, split_by, equalize, design)
   } else if (!argument_exists(split_by) && argument_exists(n)) {
@@ -78,6 +78,55 @@ select_stimuli <- function(
   # remove non-selected stimuli
   data$SET <- groups
   data[!is.na(groups), ]
+}
+
+# Internal function for subset selection based on preclustering - then anticlustering
+subset_minmax_anticlustering <- function(data, split_by, equalize, design, n) {
+  N <- nrow(data)
+  message("Starting stimulus selection using `Subset Min-Max-Anticlustering`.")
+  message("Selecting ", design, " groups, each having ", 
+          n, " elements from a pool of ", N, " stimuli.")
+
+  # keep track which items are selected; use ID for this
+  data$id_anticlustering <- 1:N
+  # Select a number of elements that can evenly be split into preclusters
+  preselection <- data[sample(1:N, size = N - (N %% design)), ]
+
+  # Compute preclusters
+  preclusters <- balanced_clustering(
+    scale(preselection[, equalize]), 
+    K = nrow(preselection) / design
+  )
+  
+  # Get best clusters - similar wrt `equalize`; dissimilar wrt `split_by`
+  distances <- by(preselection[, equalize], preclusters, dist)
+  distances2 <- by(preselection[, split_by], preclusters, dist)
+  objectives <- sapply(distances, sum)
+  objectives2 <- sapply(distances2, sum)
+  objectives <- objectives - objectives2
+  
+  needed_n <- design * n
+  needed_clusters <- needed_n / table(preclusters)[1]
+  # Best preclusters
+  most_similar_clusters <- as.numeric(names(sort(objectives))[1:needed_clusters])
+  
+  # encode items that are selected (all that are in the best preclusters)
+  is_in_output <- preclusters %in% most_similar_clusters
+  preselection <- preselection[is_in_output, ]
+  preclusters  <- preclusters[is_in_output]
+
+  # now optimize assignment using exchange method
+  anticlustering(
+    features = scale(preselection[, equalize]),
+    K = K,
+    iv = scale(preselection[, split_by]),
+    categories = preclusters
+  )
+  
+  # prepare output: Needs to incorporate NA for non-selected items
+  output <- rep(NA, N)
+  output[preselection$id_anticlustering] <- anticlusters
+  output
 }
 
 # Internal function for subset selection based on preclustering - then anticlustering
