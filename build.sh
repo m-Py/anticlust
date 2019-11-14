d53194b18ceaafd299ab2d7b7e5366e702debd80
Martin Papenberg <martin.papenberg@hhu.de>
2019-11-14 14:14:13 +0100

    building now transfers vignette to other git repo

--- build.sh
+++ build.sh
@@ -2,3 +2,13 @@
 #!/bin/sh
 
 R -e "source('build-package.R')"
+
+# copy vignette to my website
+cp ./inst/doc/my-vignette.html ../m-Py.github.io/anticlust/stimulus-selection.html
+
+cd ../m-Py.github.io/anticlust
+
+git add stimulus-selection.html
+#git commit -m "added latest version of vignette"
+
+cd -
