56506817af10bdd914eec0c5a4dd5d78143f3fde
Martin Papenberg <martin.papenberg@hhu.de>
2019-11-19 16:33:57 +0100

    merged subset min-max anticlustering and subset anticlustering functions

--- R/select_stimuli.R
+++ R/select_stimuli.R
@@ -29,8 +29,6 @@
 #'     \code{data} that should be different between sets
 #' @param equalize Character vector, the names of the variables in 
 #'     \code{data} that should be similar across sets
-#' @param balance One or more categorical variables. The frequency of
-#'     the categories is balanced between sets.
 #' @param design Specifies the number of groups per \code{split_by} 
 #'     feature. Is a vector of length \code{ncol(split_by)} (or of length
 #'     1 if only one - or no - \code{split_by} feature is passed).
@@ -51,16 +49,15 @@ select_stimuli <- function(
   data, 
   split_by = NULL, 
   equalize, 
-  balance = NULL,
   design, 
   n = NULL
 ) {
   if (argument_exists(split_by) && argument_exists(n)) {
-    groups <- subset_minmax_anticlustering(data, split_by, equalize, design, n)
+    groups <- subset_anticlustering(data, split_by, equalize, design, n)
   } else if (argument_exists(split_by) && !argument_exists(n)) {
     groups <- min_max_anticlustering(data, split_by, equalize, design)
   } else if (!argument_exists(split_by) && argument_exists(n)) {
-    groups <- subset_anticlustering(data, equalize, balance, design, n)
+    groups <- subset_anticlustering(data, split_by, equalize, design, n)
   } else if (!argument_exists(split_by) && !argument_exists(n)) {
     groups <- wrap_anticlustering(data, equalize, design)
   }
@@ -70,12 +67,10 @@ select_stimuli <- function(
   data[!is.na(groups), ]
 }
 
-# Internal function for subset selection based on preclustering - then anticlustering
-subset_minmax_anticlustering <- function(data, split_by, equalize, design, n) {
+# Internal function for subset selection based on preclustering - then 
+# anticlustering. if `split_by` exists, min-max anticlustering is conducted
+subset_anticlustering <- function(data, split_by, equalize, design, n) {
   N <- nrow(data)
-  message("Starting stimulus selection using `Subset Min-Max-Anticlustering`.")
-  message("Selecting ", design, " groups, each having ", 
-          n, " elements from a pool of ", N, " stimuli.")
 
   # keep track which items are selected; use ID for this
   data$id_anticlustering <- 1:N
@@ -90,14 +85,20 @@ subset_minmax_anticlustering <- function(data, split_by, equalize, design, n) {
   
   # Get best clusters - similar wrt `equalize`; dissimilar wrt `split_by`
   distances <- by(preselection[, equalize], preclusters, dist)
-  distances2 <- by(preselection[, split_by], preclusters, dist)
   objectives <- sapply(distances, sum)
-  objectives2 <- sapply(distances2, sum)
-  objectives <- objectives - objectives2
-  
+    
+  # Some additional work needs to be done if min-max anticlustering is required
+  min_max <- ""
+  if (argument_exists(split_by)) {
+    min_max <- "Min-Max-"
+    distances2 <- by(preselection[, split_by], preclusters, dist)
+    objectives2 <- sapply(distances2, sum)
+    objectives <- objectives - objectives2
+  }
+    
   needed_n <- design * n
   needed_clusters <- needed_n / table(preclusters)[1]
-  # Best preclusters
+  # Best preclusters - (TODO: insert stochastic component)
   most_similar_clusters <- as.numeric(names(sort(objectives))[1:needed_clusters])
   
   # encode items that are selected (all that are in the best preclusters)
@@ -105,11 +106,19 @@ subset_minmax_anticlustering <- function(data, split_by, equalize, design, n) {
   preselection <- preselection[is_in_output, ]
   preclusters  <- preclusters[is_in_output]
 
+  iv <- NULL
+  if (argument_exists(split_by)) {
+    iv <- scale(preselection[, split_by])
+  }
+
+  message("Starting stimulus selection using `Subset ", min_max, " Anticlustering`.")
+  message("Selecting ", design, " groups, each having ", 
+          n, " elements from a pool of ", N, " stimuli.")
   # now optimize assignment using exchange method
   anticlusters <- anticlustering(
     features = scale(preselection[, equalize]),
     K = prod(design),
-    iv = scale(preselection[, split_by]),
+    iv = iv,
     categories = preclusters
   )
   
@@ -119,58 +128,6 @@ subset_minmax_anticlustering <- function(data, split_by, equalize, design, n) {
   output
 }
 
-# Internal function for subset selection based on preclustering - then anticlustering
-subset_anticlustering <- function(data, equalize, balance, design, n) {
-  N <- nrow(data)
-  message("Starting stimulus selection using `Subset Anticlustering`.")
-  message("Selecting ", design, " groups, each having ", 
-          n, " elements from a pool of ", N, " stimuli.")
-
-  # keep track which items are selected; use ID for this
-  data$id_anticlustering <- 1:N
-  # Select a number of elements that can evenly be split into preclusters
-  preselection <- data[sample(1:N, size = N - (N %% design)), ]
-
-  # Compute preclusters
-  preclusters <- balanced_clustering(
-    scale(preselection[, equalize]), 
-    K = nrow(preselection) / design
-  )
-  
-  # Get most similar clusters
-  distances <- by(preselection[, equalize], preclusters, dist)
-  objectives <- sapply(distances, sum)
-  needed_n <- design * n
-  needed_clusters <- needed_n / table(preclusters)[1]
-  # Best preclusters
-  most_similar_clusters <- as.numeric(names(sort(objectives))[1:needed_clusters])
-  
-  # encode items that are selected (all that are in the best preclusters)
-  is_in_output <- preclusters %in% most_similar_clusters
-  preselection <- preselection[is_in_output, ]
-  preclusters  <- preclusters[is_in_output]
-
-  # if a categorical variable is passed, use this for the initial 
-  # assignment and exchange method; can't use both categorical and
-  # preclustering restrictions (only in very specific cases - I should
-  # implement the test `preclustering_possible()` and 
-  # `precluster_per_category()` for this case
-  if (argument_exists(balance)) {
-    preclusters  <- merge_into_one_variable(preselection[, balance])
-  }
-  
-  # now optimize assignment using exchange method
-  anticlusters <- anticlustering(
-    scale(preselection[, equalize]),
-    K = design,
-    categories = preclusters
-  )
-  
-  # prepare output: Needs to incorporate NA for non-selected items
-  output <- rep(NA, N)
-  output[preselection$id_anticlustering] <- anticlusters
-  output
-}
 
 # Internal function for min-max anticlustering
 min_max_anticlustering <- function(data, split_by, equalize, design) {
