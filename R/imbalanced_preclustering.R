c8e161eb8b216a4c8f4dab8df0f01bd806730bde
Martin Papenberg <martin.papenberg@hhu.de>
2019-11-20 09:57:08 +0100

    imbalanced preclustering now deals with very small input data

--- R/imbalanced_preclustering.R
+++ R/imbalanced_preclustering.R
@@ -4,15 +4,21 @@
 # @param K the Number of anticlusters (! -> the number of clusters returned is ~ nrow(features) / K)
 
 imbalanced_preclustering <- function(features, K) {
-  features <- as.matrix(features)
+  features <- data.frame(features)
   N <- nrow(features)
+  features$preclusters_id <- 1:N
   preclusters <- rep(NA, N)
   # only select as many data as can be clustered into balanced clusters
   subsetted <- remove_outliers(features, 1:ncol(features), K)
-  preclusters[1:nrow(subsetted)] <- balanced_clustering(
-    subsetted,
-    K = nrow(subsetted) / K
-  )
+  # test if only one cluster can be filled:
+  if (nrow(subsetted) == K)  {
+    preclusters[subsetted$preclusters_id] <- 1
+  } else {
+    preclusters[subsetted$preclusters_id] <- balanced_clustering(
+      subsetted,
+      K = nrow(subsetted) / K
+    )
+  }
   # the remainders get into the last cluster
   if (sum(is.na(preclusters)) > 0) {
     preclusters[is.na(preclusters)] <- max(preclusters, na.rm = TRUE) + 1
