bc612b17aec54089fab625e80a886086f6870162
Martin Papenberg <martin.papenberg@hhu.de>
2019-12-10 17:39:29 +0100

    guess this is cleaner

--- R/nn-centroid-clustering.R
+++ R/nn-centroid-clustering.R
@@ -6,18 +6,19 @@
 nn_centroid_clustering <- function(data, K) {
   # compute the distances between all elements and overall cluster center
   centroid <- t(as.matrix(colMeans(data)))
+  distances <- c(dist_from_centers(data, centroid, squared = FALSE))
   counter <- 1
   idx <- 1:nrow(data)
   clusters <- rep(NA, nrow(data))
   
   while (nrow(data) > 0) {
-    distances <- c(dist_from_centers(data, centroid, squared = FALSE))
     # get element with maximum distance to centroid
     max_away <- which.max(distances)[1]
     # compute nearest neighbors for element that is furthest away
     clustered <- nn2(data, data[max_away, ], K)$nn.idx
     clusters[idx[clustered]] <- counter
     data <- data[-clustered, ]
+    distances <- distances[-clustered]
     idx  <- idx[-clustered]
     counter <- counter + 1
   }
