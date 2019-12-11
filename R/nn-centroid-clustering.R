2a5c7a129ab34994febc235865eb2ccf4f8a7f51
Martin Papenberg <martin.papenberg@hhu.de>
2019-12-11 08:59:10 +0100

    using mean imputation if not otherwise possible

--- R/nn-centroid-clustering.R
+++ R/nn-centroid-clustering.R
@@ -5,6 +5,10 @@
 # K = size of the small groups
 nn_centroid_clustering <- function(data, K) {
   data <- as.matrix(data)
+  if (sum(is.na(data)) != 0) {
+    data <- impute_mean_na(data)
+  }
+  
   distances <- distances_from_centroid(data)
   counter <- 1
   idx <- 1:nrow(data)
@@ -31,4 +35,12 @@ nn_centroid_clustering <- function(data, K) {
 distances_from_centroid <- function(data) {
   centroid <- t(as.matrix(colMeans(data)))
   c(dist_from_centers(data, centroid, squared = FALSE))
+}
+
+# for NA values use, mean imputation
+impute_mean_na <- function(data) {
+  warning("I used mean imputation for NA values; you really should ",
+          "consider if you are okay with having missing ",
+          "values in your data!")
+  apply(data, 2, function(x) ifelse(is.na(x), mean(x, na.rm = TRUE), x))
 }
