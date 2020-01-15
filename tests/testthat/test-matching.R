25aaf3c47c1b7d6e49ea56de3c5c07019dfb44cb
Martin Papenberg <martin.papenberg@hhu.de>
2020-01-15 14:49:06 +0100

    added tests for matching function

---
+++ tests/testthat/test-matching.R
@@ -0,0 +1,75 @@
+
+
+context("Test matching function")
+library("anticlust")
+
+test_that("Matching function behaves correctly with `p` argument", {
+  # generate some random data
+  m <- sample(1:4, size = 1)
+  n <- sample(10:100, size = 1)
+  data <- matrix(rnorm(n * m), ncol = m)
+  p <- sample(2:5, size = 1)
+  
+  # feature input
+  # test that matches are of size p
+  matches <- matching(data, p = p)
+  expect_true(all(table(matches) == p))
+  # non-fitting elements have NA
+  expect_equal(sum(is.na(matches)), n %% p)
+  # are all matches objective sorted by distance objective?
+  objectives <- sapply(
+    1:max(matches, na.rm = TRUE), 
+    function(x) sum(dist(data[!is.na(matches) & matches == x, ]))
+  )
+  expect_true(!is.unsorted(objectives))
+  
+  # repeat the above for distance input
+  data <- as.matrix(dist(data))
+  matches <- matching(distances = data, p = p)
+  expect_true(all(table(matches) == p))
+  expect_equal(sum(is.na(matches)), n %% p)
+  objectives <- sapply(
+    1:max(matches, na.rm = TRUE), 
+    function(x) sum(as.dist(data[!is.na(matches) & matches == x, !is.na(matches) & matches == x]))
+  )
+  expect_true(!is.unsorted(objectives))
+})
+
+test_that("Matching function behaves correctly with `groups` argument", {
+  # generate some random data
+  m <- sample(1:4, size = 1)
+  n <- sample(20:100, size = 1)
+  data <- matrix(rnorm(n * m), ncol = m)
+  p <- sample(2:5, size = 1)
+  groups <- sample(1:p, size = n, replace = TRUE)
+  while (any(table(groups) < 2)) {
+    groups <- sample(1:p, size = n, replace = TRUE)
+  }
+  
+  # feature input
+  # test that matches are of size 
+  matches <- matching(data, groups = groups)
+  expect_true(all(table(matches) == p))
+  # non-fitting elements have NA
+  n_matched <- (min(table(groups)) * p) # how many elements were matched
+  expect_equal(sum(is.na(matches)), n - n_matched)
+  # are all matches objective sorted by distance objective?
+  objectives <- sapply(
+    1:max(matches, na.rm = TRUE), 
+    function(x) sum(dist(data[!is.na(matches) & matches == x, ]))
+  )
+  expect_true(!is.unsorted(objectives))
+  
+  # repeat the above for distance input
+  data <- as.matrix(dist(data))
+  matches <- matching(data, groups = groups)
+  expect_true(all(table(matches) == p))
+  # non-fitting elements have NA
+  n_matched <- (min(table(groups)) * p) # how many elements were matched
+  expect_equal(sum(is.na(matches)), n - n_matched)
+  objectives <- sapply(
+    1:max(matches, na.rm = TRUE), 
+    function(x) sum(as.dist(data[!is.na(matches) & matches == x, !is.na(matches) & matches == x]))
+  )
+  expect_true(!is.unsorted(objectives))
+})
