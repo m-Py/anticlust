abf6eff38cb38545905bbb1c8a251e9081715105
Martin Papenberg <martin.papenberg@hhu.de>
2020-02-23 13:55:37 +0100

    added test for number of exchange partners in k-means

--- tests/testthat/test-exchange-partner-generation.R
+++ tests/testthat/test-exchange-partner-generation.R
@@ -47,3 +47,53 @@ test_that("Exchange partners are generated correctly - preclustering and categor
   partners <- get_exchange_partners(clusters, i, constraints)
   expect_equal(partners == 4, TRUE)
 })
+
+
+test_that("Exchange partners are generatede correctly for fast k-means method", {
+  # function that tests that all exchange partners are from the same category
+  test_idx <- function(idx, categories) {
+    if (argument_exists(categories)) {
+      for (i in 1:length(idx)) {
+        partners <- categories[idx[[i]]]
+        expect_true(all(partners == partners[1]))
+      }
+    }
+  }
+  
+  ### Using categorical restrictions
+  # Case 1: restricted number of exchange partners (i.e., nearest neighbour search)
+  categories <- to_numeric(schaper2019$room)
+  partners <- all_exchange_partners(
+    features = schaper2019[, 3:6],
+    k_neighbours = 10,
+    categories = categories
+  )
+  test_idx(partners, categories)
+  
+  # Case 2: no restriction on number of exchange partners
+  partners <- all_exchange_partners(
+    features = schaper2019[, 3:6],
+    k_neighbours = Inf,
+    categories = categories
+  )
+  test_idx(partners, categories)
+  
+  ### Not using categorical restrictions
+  # Case 1: restricted number of exchange partners (i.e., nearest neighbour search)
+  categories <- to_numeric(schaper2019$room)
+  k_neighbours <- 10
+  partners <- all_exchange_partners(
+    features = schaper2019[, 3:6],
+    k_neighbours = k_neighbours,
+    categories = NULL
+  )
+  expect_true(all(sapply(partners, length) == k_neighbours + 1))
+  
+  # Case 2: no restriction on number of exchange partners
+  partners <- all_exchange_partners(
+    features = schaper2019[, 3:6],
+    k_neighbours = Inf,
+    categories = NULL
+  )
+  expect_true(all(sapply(partners, length) == nrow(schaper2019)))
+})
