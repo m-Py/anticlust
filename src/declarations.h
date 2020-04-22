cf0d00b73db3171e1f68b4985fc0d9ce290c058a
Martin Papenberg <martin.papenberg@hhu.de>
2020-04-22 11:55:28 +0200

    formatting h-file

--- src/declarations.h
+++ src/declarations.h
@@ -42,7 +42,7 @@ void compute_center(
         struct node *HEAD, 
         int frequency
 );
-void fill_data_points(
+int fill_data_points(
         double *data, 
         size_t n, 
         size_t m, 
@@ -50,20 +50,46 @@ void fill_data_points(
         int *clusters
 );
 double cluster_variance(
-                size_t m, 
-                struct node *HEAD, 
-                double center[m]
+        size_t m, 
+        struct node *HEAD, 
+        double center[m]
 );
-void swap(size_t n, size_t i, size_t j, 
-          struct node *PTR_NODES[n]);
-void update_head(size_t k, struct node *one, struct node *two, 
-                 struct node *PTR_CLUSTER_HEADS[k]);
-void update_centers(size_t k, size_t m, double CENTERS[k][m],
-		    struct node *one, struct node *two,
-		    int *frequencies);
-double update_objective(size_t k, size_t m, double centers[k][m], size_t cl1, 
-                        size_t cl2, struct node *head_array[k], 
-                        double VAR_OBJECTIVE[k]);
-void print_elements(size_t n, size_t m, struct node **ptr_to_nodes);
-void cp_array(size_t n, double origin[n], double target[n]);
-void cp_matrix(size_t n, size_t m, double origin[n][m], double target[n][m]);
+void swap(
+        size_t n, 
+        size_t i, 
+        size_t j, 
+        struct node *PTR_NODES[n]
+);
+void update_centers(
+        size_t k, 
+        size_t m, 
+        double CENTERS[k][m],
+        struct node *one, 
+        struct node *two,
+        int *frequencies
+);
+double update_objective(
+        size_t k, 
+        size_t m, 
+        double centers[k][m], 
+        size_t cl1, 
+        size_t cl2, 
+        struct node *head_array[k], 
+        double VAR_OBJECTIVE[k]
+);
+void print_elements(
+        size_t n, 
+        size_t m, 
+        struct node **ptr_to_nodes
+);
+void cp_array(
+        size_t n, 
+        double origin[n], 
+        double target[n]
+);
+void cp_matrix(
+        size_t n, 
+        size_t m, 
+        double origin[n][m],
+        double target[n][m]
+);
