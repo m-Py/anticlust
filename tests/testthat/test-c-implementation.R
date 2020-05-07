8e1ee56dbff269605b5f1acd81b8502e6f7afd71
Martin Papenberg <martin.papenberg@hhu.de>
2020-05-07 09:30:25 +0200

    test should be against variance_objective_
    - because in future releases, the standard objective "variance" will likely rely on the C implementation
    
    - need some tolerance for numeric precision because local updating and recomputing sometimes result in different updated values (after many decimals, but this does affect the behaviour of the exchange algorithm)

--- tests/testthat/test-c-implementation.R
+++ tests/testthat/test-c-implementation.R
@@ -9,8 +9,12 @@ test_that("C implemenation has same output as R implementation", {
   features <- matrix(rnorm(N * M), ncol = M)
   clusters <- initialize_clusters(N, K, NULL)
   cl1 <- fanticlust(features, clusters)
-  cl2 <- anticlustering(features, clusters, objective = "variance")
-  expect_true(all(cl1 == cl2))
+  cl2 <- anticlustering(features, clusters, variance_objective_)
+  expect_equal(
+    variance_objective_(cl1, features),
+    variance_objective_(cl2, features),
+    tolerance = 0.1
+  ) # some tolerance for numeric precision
   
   # now also use a categorical constraint
   C <- sample(2:5, size = 1)
@@ -20,11 +24,15 @@ test_that("C implemenation has same output as R implementation", {
   cl2 <- anticlustering(
     features, 
     clusters, 
-    objective = "variance", 
+    objective = variance_objective_, 
     categories = categories
   )
-  expect_true(all(cl1 == cl2))
-  
+  expect_equal(
+    variance_objective_(cl1, features),
+    variance_objective_(cl2, features),
+    tolerance = 0.1
+  ) # some tolerance for numeric precision
+
   # TODO: include a test for the edge cases (i.e., categories with 1 or N members)
   
 })
