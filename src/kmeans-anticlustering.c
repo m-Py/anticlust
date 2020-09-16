98b8e5e19b1a28b5a5a844fa496be0f433fa2d10
Martin Papenberg <martin.papenberg@hhu.de>
2020-09-16 15:24:37 +0200

    white space

--- src/kmeans-anticlustering.c
+++ src/kmeans-anticlustering.c
@@ -125,7 +125,7 @@ void kmeans_anticlustering(double *data, int *N, int *M, int *K, int *frequencie
         size_t *CATEGORY_HEADS[c];
         
         mem_error_categories = get_indices_by_category(
-                n, c, CATEGORY_HEADS, USE_CATS,                                     categories, CAT_frequencies, POINTS
+                n, c, CATEGORY_HEADS, USE_CATS, categories, CAT_frequencies, POINTS
         );
         
         if (mem_error_categories == 1) {
