b771c673081860920239cb354c7411d49ba12815
Martin Papenberg <martin.papenberg@hhu.de>
2020-09-17 09:35:53 +0200

    test of C implementation

---
+++ tests/testthat/test-c-implementation.R
@@ -0,0 +1,82 @@
+
+context("C implementation")
+library("anticlust")
+
+test_that("C implemenation of Anticlustering has same output as R implementation", {
+  set.seed(123)
+  N <- sample(40:80, size = 1)
+  M <- sample(1:5, size = 1)
+  K <- sample(2:5, size = 1)
+  features <- matrix(rnorm(N * M), ncol = M)
+  clusters <- initialize_clusters(N, K, NULL)
+  cl1 <- anticlustering(features, clusters, objective = "variance")
+  cl2 <- anticlustering(features, clusters, variance_objective)
+  expect_true(all(cl1 == cl2))
+
+  # now also use a categorical constraint
+  set.seed(71207)
+  C <- sample(2:5, size = 1)
+  categories <- sample(C, size = N, replace = TRUE)
+  clusters <- initialize_clusters(N, K, categories)
+  cl1 <- anticlustering(
+    features, 
+    clusters, 
+    objective = "variance", 
+    categories = categories
+  )
+  cl2 <- anticlustering(
+    features, 
+    clusters, 
+    objective = variance_objective, 
+    categories = categories
+  )
+  expect_true(all(cl1 == cl2))
+
+  # TODO: include a test for the edge cases (i.e., categories with 1 or N members)
+  
+  # Use diversity objective
+  set.seed(123)
+  clusters <- initialize_clusters(N, K, NULL)
+  cl1 <- anticlustering(features, clusters, objective = "diversity")
+  cl2 <- anticlustering(features, clusters, diversity_objective)
+  expect_true(all(cl1 == cl2))
+  
+  # now also use a categorical constraint
+  set.seed(71207)
+  C <- sample(2:5, size = 1)
+  categories <- sample(C, size = N, replace = TRUE)
+  clusters <- initialize_clusters(N, K, categories)
+  cl1 <- anticlustering(
+    features, 
+    clusters, 
+    objective = "diversity", 
+    categories = categories
+  )
+  cl2 <- anticlustering(
+    features, 
+    clusters, 
+    objective = diversity_objective, 
+    categories = categories
+  )
+  expect_true(all(cl1 == cl2))
+  
+  # now also use preclustering
+  set.seed(71207)
+  C <- sample(2:5, size = 1)
+  categories <- sample(C, size = N, replace = TRUE)
+  clusters <- initialize_clusters(N, K, categories)
+  cl1 <- anticlustering(
+    features, 
+    clusters, 
+    objective = "diversity", 
+    categories = categories
+  )
+  cl2 <- anticlustering(
+    features, 
+    clusters, 
+    objective = diversity_objective, 
+    categories = categories
+  )
+  expect_true(all(cl1 == cl2))
+
+})
