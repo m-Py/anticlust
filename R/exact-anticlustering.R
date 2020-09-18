e6fdae50965150781d1f4621844f24c63167364a
Martin Papenberg <martin.papenberg@hhu.de>
2020-09-18 16:47:43 +0200

    must be converted to matrix here

--- R/exact-anticlustering.R
+++ R/exact-anticlustering.R
@@ -49,7 +49,7 @@ convert_to_distances <- function(data) {
   if (!is_distance_matrix(data)) {
     distances <- as.matrix(dist(data))
   } else {
-    distances <- data
+    distances <- as.matrix(data)
   }
   distances
 }
