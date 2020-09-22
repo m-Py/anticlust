b279dd4c9978b30e45c2b2f2572160cf42834ed5
Martin Papenberg <martin.papenberg@hhu.de>
2020-09-22 16:46:29 +0200

    update on test for repeated exchange method

--- tests/testthat/test-repeated.exchange.R
+++ tests/testthat/test-repeated.exchange.R
@@ -2,10 +2,9 @@
 context("Test repeated exchange methods")
 library("anticlust")
 
-test_that("all argument combinations run through", {
+test_that("argument combinations run through (no preclustering / categories)", {
   methods <- c("exchange", "local-maximum")
   conditions <- expand.grid(
-    preclustering = c(TRUE, FALSE),
     method = methods,
     repetitions = c(1, 10),
     objective = c("variance", "diversity"),
@@ -16,36 +15,25 @@ test_that("all argument combinations run through", {
   
   # Set up matrix to store the objective values obtained by different methods
   K <- sample(2:6, size = 1)
-  N <- K * sample(10:20, size = 1)
+  N <- K * sample(7:14, size = 1)
   M <- sample(1:10, size = 1)
   features <- matrix(rnorm(N * M), ncol = M)
   distances <- dist(features)
   objs <- rep(NA, nrow(conditions)) # store objectives
-  initial_clusters <- sample(rep_len(1:K, length.out = N))
-  preclusters <- matching(features, K)
-  initial_clusters_preclustered <- initialize_clusters(N, K, preclusters)
-  
+  clusters <- sample(rep_len(1:K, length.out = N))
+
   for (i in 1:nrow(conditions)) {
     if (conditions$input[i] == "distance") {
       data <- distances
     } else {
       data <- features
     }
-    
-    if (conditions$preclustering[i] == TRUE) {
-      categories <- preclusters
-      clusters <- initial_clusters_preclustered
-    } else {
-      categories <- NULL
-      clusters <- initial_clusters
-    }
-    
+
     anticlusters <- anticlustering(
       features, 
       K = clusters,
       objective = conditions$objective[i],
       method = conditions$method[i],
-      categories = preclusters,
       repetitions = conditions$repetitions[i]
     )
     
@@ -65,16 +53,18 @@ test_that("all argument combinations run through", {
     conditions, 
     direction = "wide", 
     timevar = "repetitions",
-    idvar = colnames(conditions)[c(1, 2, 4, 5)]
+    idvar = colnames(conditions)[c(1, 3, 4)]
   )
-  wide1
+  # test that local maximum search improves results
+  expect_true(all(wide1[, 5] >= wide1[, 4]))
+  
   wide2 <- reshape(
     conditions, 
     direction = "wide", 
     timevar = "method",
-    idvar = colnames(conditions)[c(1, 3, 4, 5)]
+    idvar = colnames(conditions)[2:4]
   )
-  wide2
-  
-  
+  # test that repetitions improve results
+  expect_true(all(wide2[, 5] >= wide2[, 4]))
+
 })
