a097c395bf321520a6e9b5a57689cc2d00ebccee
Martin Papenberg <martin.papenberg@hhu.de>
2021-05-18 10:09:53 +0200

    some comments and variable renaming

--- R/initialize_not_split_clustering.R
+++ R/initialize_not_split_clustering.R
@@ -1,37 +1,47 @@
 
 # only works for equal-sized groups! (or does it?)
-clustering_fit <- function(not_split, cl) {
-  not_split <- c(not_split)
+clustering_fit_must_link <- function(must_link, cl) {
+  must_link <- c(must_link)
   K <- length(unique(cl))
-  # each not_split group should be in the same cluster:
-  tab <- table(cl, not_split)
-  # i.e., in tab, each column should have only zeros except for one row
-  (colSums(table(cl, not_split) == 0) - (K - 1))^2
+  # each not_split group should be in the same cluster
+  # (# i.e., in tab, each column should have only zeros except for one row):
+  tab <- table(cl, must_link)
+  # compute squared difference between the number of zeros in each column
+  # and the number of zeros that should be there; this is the objective function
+  # that has to be minimized
+  (colSums(table(cl, must_link) == 0) - (K - 1))^2
 }
 
-obj_fun <- function(x, cl) {
-  clustering_fit <- clustering_fit(x, cl)
+# Expand the above objective function, return Infinity when the clustering
+# fits perfectly.
+obj_fun_must_link <- function(x, cl) {
+  clustering_fit <- clustering_fit_must_link(x, cl)
   if (all(clustering_fit == 0)) {
     return(Inf)
   }
   sum(clustering_fit) * (-1) # clustering fit has to be minimized
 }
 
-initialize_with_not_split <- function(not_split, N, K) {
-  df <- data.frame(order = 1:N, not_split = not_split)
-  df <- df[order(df$not_split), ]
+# Initialize a clustering vector satisfying must-link constraints.
+# If an initial guess does not work, uses an exchange process to optimize 
+# the above objective function representing the degree to which the constraints
+# are satisfied.
+initialize_must_link_clustering <- function(must_link, N, K) {
+  df <- data.frame(order = 1:N, must_link = must_link)
+  df <- df[order(df$must_link), ]
   df$cl <- sort(rep_len(1:K, N))
-  if (all(clustering_fit(df$not_split, df$cl) == 0)) {
+  if (all(clustering_fit_must_link(df$must_link, df$cl) == 0)) {
     print("success")
     return(df[order(df$order), ]$cl)
   }
   # if initial assignment does not work, try to optimize!
-  df$cl <- anticlustering(
-    df$not_split, 
+  df$cl <- exchange_method(
+    as.matrix(df$must_link), 
     K = df$cl, 
-    objective = obj_fun,
+    obj_function = obj_fun_must_link,
+    categories = NULL
   )
-  if (all(clustering_fit(df$not_split, df$cl) == 0)) {
+  if (all(clustering_fit_must_link(df$must_link, df$cl) == 0)) {
     print("success after optimization")
   } else {
     print("no success")
