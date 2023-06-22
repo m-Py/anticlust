6c2315ca367019cbae7d8a8dee8caf8c31316903
Martin Papenberg <martin.papenberg@hhu.de>
2023-06-22 14:59:39 +0200

    remove rownames from output

--- R/Optimal_Dispersion.R
+++ R/Optimal_Dispersion.R
@@ -186,7 +186,7 @@ optimal_dispersion <- function(x, K, solver = NULL, max_dispersion_considered = 
     list(
       dispersion = dispersion, 
       groups = groups$groups,
-      edges = all_nns,
+      edges = unname(all_nns), # rownames can be quite ugly here
       dispersions_considered = c(dispersions_considered, dispersion)
     )
   )
