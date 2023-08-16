82472e0d8afda9d3b07a5ca7110d6c23ef31c2ea
Martin Papenberg <martin.papenberg@hhu.de>
2023-08-16 21:29:11 +0200

    rows are partners, columns are elements

--- R/c-anticlustering.R
+++ R/c-anticlustering.R
@@ -96,7 +96,7 @@ c_anticlustering <- function(data, K, categories = NULL, objective, exchange_par
       as.integer(frequencies),
       clusters = as.integer(clusters),
       as.integer(exchange_partners),
-      as.integer(ncol(exchange_partners)),
+      as.integer(nrow(exchange_partners)),
       mem_error = as.integer(0),
       PACKAGE = "anticlust"
     )
