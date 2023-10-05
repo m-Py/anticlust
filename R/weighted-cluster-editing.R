c9920ccda5e9bc5f34822cc902c8a8ffe27ea907
Martin Papenberg <martin.papenberg@hhu.de>
2023-10-05 16:18:42 +0200

    docs

--- R/weighted-cluster-editing.R
+++ R/weighted-cluster-editing.R
@@ -20,9 +20,9 @@
 #' features <- swiss
 #' distances <- dist(scale(swiss))
 #' hist(distances)
-#' # Define agreement as being close enough to each other.
-#' # By defining low agreement as -1 and high agreement as +1, we
-#' # solve *unweighted* cluster editing
+#' # Define agreement as being close enough to each other according to 
+#' # the Euclidean distance. By coding low agreement as -1 and high agreement 
+#' # as +1, we solve *unweighted* cluster editing
 #' agreements <- ifelse(as.matrix(distances) < 3, 1, -1)
 #' clusters <- wce(agreements)
 #' plot(swiss, col = clusters, pch = 19)
