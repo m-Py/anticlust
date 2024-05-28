8fc62afbf1564d0341243dfa292faec1866a9fc8
Martin Papenberg <martin.papenberg@hhu.de>
2024-05-28 16:51:12 +0200

    do not use restricted search for BILS, it does not need it

--- R/cannot_link_anticlustering.R
+++ R/cannot_link_anticlustering.R
@@ -36,9 +36,6 @@ cannot_link_anticlustering <- function(x, init_clusters, cannot_link, objective,
     objective <- "diversity"
   }
 
-  # set cannot-link distances to large negative value so they cannot be linked
-  x[rbind(cannot_link, t(apply(cannot_link, 1, rev)))] <- -(sum(x) + 1)
-  
   ## special case of only one init partition...
   init_clusters <- as.matrix(init_clusters)
   if (ncol(init_clusters) == 1) {
@@ -50,11 +47,14 @@ cannot_link_anticlustering <- function(x, init_clusters, cannot_link, objective,
     init_clusters_bils <- init_clusters
     init_clusters <- init_clusters - 1 # -1 for C
   }
-
+  
   if (method == "brusco") {
-    return(BILS_E_ALL_RESTRICTED(x, init_clusters_bils, cannot_link))
+    return(BILS_CANNOT_LINK(x, init_clusters_bils, cannot_link))
   }
   
+  # For LCW: Set cannot-link distances to large negative value so they cannot be linked
+  x[rbind(cannot_link, t(apply(cannot_link, 1, rev)))] <- -(sum(x) + 1)
+
   c_anticlustering(
     x, 
     K = K, 
@@ -68,16 +68,19 @@ cannot_link_anticlustering <- function(x, init_clusters, cannot_link, objective,
 
 
 # Wrapper for BILS method that preserves optimal dispersion and potentially uses multiple initial partitions
-BILS_E_ALL_RESTRICTED <- function(distances, init_clusters, cannot_link) {
+# TODO check out this function, it should be able to use more ILS repetitions...
+BILS_CANNOT_LINK <- function(distances, init_clusters, cannot_link) {
+  N <- nrow(distances)
   multiple_partitions_as_input <- is.matrix(init_clusters)
+  selection_matrix <- matrix(1, ncol = N, nrow = N)
+  selection_matrix[rbind(cannot_link, rev(cannot_link))] <- -1
   PARTITIONS <- bicriterion_anticlustering(
     distances, 
     K = if (multiple_partitions_as_input) init_clusters[1, ] else init_clusters,
     R = if (multiple_partitions_as_input) c(nrow(init_clusters), 1) else c(1, 1),
-    init_partitions = if (multiple_partitions_as_input) init_clusters else NULL
+    init_partitions = if (multiple_partitions_as_input) init_clusters else NULL,
+    dispersion_distances = selection_matrix
   )
-  ## TODO: THE RETURNED PARTITION MUST BE DECIDED ON THE BASIS OF A CANNOT-LINK MATRIX,
-  ## NOT THE DISPERSION OF THE ORIGINAL DISTANCES!!!!
-  PARTITIONS[which.max(apply(PARTITIONS, 1, dispersion_objective, x = distances)), ]
+  PARTITIONS[which.max(apply(PARTITIONS, 1, dispersion_objective, x = selection_matrix)), ]
 }
 
