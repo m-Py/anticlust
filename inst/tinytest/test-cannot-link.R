1d1d294845747c5aeb49ad5cac87722aca159a6e
Martin Papenberg <martin.papenberg@hhu.de>
2024-05-28 16:51:42 +0200

    more cannot-link tests

--- inst/tinytest/test-cannot-link.R
+++ inst/tinytest/test-cannot-link.R
@@ -8,3 +8,29 @@ expect_true(a[1] != a[2])
 
 a <- anticlustering(rnorm(6), K = 2, cannot_link = cbind(1, 2), method = "brusco")
 expect_true(a[1] != a[2])
+
+
+# now make it harder: find instance where two items are linked in optimal solution, 
+# and then insert cannot-link constraint!
+
+N <- 14
+M <- 3
+data <- matrix(rnorm(N*M), ncol = M)
+a <- anticlustering(data, K = 2, method = "ilp")
+one <- which(a == 1)[1]
+other <- which(a == 1)[2]
+diversity_objective(data, a)
+
+b <- anticlustering(data, K = 2, cannot_link = cbind(one, other))
+expect_true(b[one] != b[other])
+expect_true(diversity_objective(data, a) >= diversity_objective(data, b))
+
+b <- anticlustering(data, K = 2, cannot_link = cbind(one, other), method = "ilp")
+expect_true(b[one] != b[other])
+expect_true(diversity_objective(data, a) >= diversity_objective(data, b))
+
+b <- anticlustering(data, K = 2, cannot_link = cbind(one, other), method = "brusco")
+expect_true(b[one] != b[other])
+expect_true(a[one] == a[other]) # this is necessary, but maybe we appreciate the reminder.
+expect_true(diversity_objective(data, a) >= diversity_objective(data, b))
+
