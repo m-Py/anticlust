46fa8be737555ec66e184ddbaaf84a896e8c0581
Martin Papenberg <martin.papenberg@hhu.de>
2024-09-09 16:17:10 +0200

    changed test directory

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -2,5 +2,5 @@
 if ( requireNamespace("tinytest", quietly=TRUE) ){
   library(tinytest)
   library(anticlust)
-  tinytest::test_package("anticlust")
+  tinytest::test_package("anticlust", testdir = "inst/tinytest")
 }
