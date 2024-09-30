7a3226b03b0edc4b2a5ef397ef2213c74dd31ea1
Martin Papenberg <martin.papenberg@hhu.de>
2024-09-30 16:33:18 +0200

    R CMD check seems to need this ...

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -2,5 +2,5 @@
 if ( requireNamespace("tinytest", quietly=TRUE) ){
   library(tinytest)
   library(anticlust)
-  tinytest::test_package("anticlust", testdir = "inst/tinytest")
+  tinytest::test_package("anticlust")
 }
