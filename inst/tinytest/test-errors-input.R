7ed07ba07d9626fde1f80a74455b029e8e2730ba
Martin Papenberg <martin.papenberg@hhu.de>
2025-09-19 17:16:22 +0200

    anticlustering can now be used with NA

--- inst/tinytest/test-errors-input.R
+++ inst/tinytest/test-errors-input.R
@@ -1,17 +1,14 @@
 
 library("anticlust")
 
-# exported function cannot be used with NA"
+# some exported function cannot be used with NA (anticlustering now can!)
 data(iris)
 iris[1, 1] <- NA
-expect_error(
-  anticlustering(iris[, -5], K = 3)
-)
 expect_error(
   matching(iris[, -5], p = 3)
 )
 expect_error(
-  balanced_clustering(iris[, -5], p = 3)
+  balanced_clustering(iris[, -5], K = 3)
 )
 
 # exported functions cannot be used with non-numeric input"
