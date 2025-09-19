285a05ce19735fe021f8e7506a4a4ba420a24d0f
Martin Papenberg <martin.papenberg@hhu.de>
2025-09-19 17:21:09 +0200

    additional tests

---
+++ inst/tinytest/test-factor-input.R
@@ -0,0 +1,17 @@
+
+library("anticlust")
+library("tinytest")
+
+# Test that using factor variables as input now works
+
+gr <- anticlustering(iris, K = 3, standardize = TRUE)
+mean_sd_tab(iris[-5], gr)
+table(gr, iris$Species)
+
+gr <- anticlustering(iris, K = 3, standardize = TRUE, method = "3phase")
+mean_sd_tab(iris[-5], gr)
+table(gr, iris$Species)
+
+gr <- anticlustering(iris, K = 5, standardize = TRUE, method = "3phase", objective = "kplus")
+mean_sd_tab(iris[-5], gr)
+table(gr, iris$Species)
