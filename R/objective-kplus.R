1889c59881a1efd4cf25a4abc36928a39ccc3618
Martin Papenberg <martin.papenberg@hhu.de>
2025-09-22 12:59:53 +0200

    remove duplicate code

--- R/objective-kplus.R
+++ R/objective-kplus.R
@@ -98,32 +98,3 @@ kplus_moment_variables <- function(x, T, standardize = TRUE) {
   }
   scale(x)
 }
-
-# function to compute features for variance
-# moment = 2 -> variance; 3 = skew; 4 = kurtosis
-moment_features <- function(data, moment = 2) {
-  apply(data, 2, function(x) (x - mean(x))^moment)
-}
-
-# Compute k-covariances features for an original number of M features.
-# That is choose(M, 2) additional features, i.e., O(M^2) additional features.
-
-covariance_features <- function(data) {
-  M <- ncol(data)
-  if (M < 2) {
-    stop("k-covariances can only be used when at least two features are present.")
-  }
-  feature_combinations <- t(combn(M, 2))
-  # store new features in matrix
-  cov_feature_matrix <- matrix(ncol = nrow(feature_combinations), nrow = nrow(data))
-  for (i in 1:nrow(feature_combinations)) {
-    f1 <- feature_combinations[i, 1]
-    f2 <- feature_combinations[i, 2]
-    cov_feature_matrix[, i] <- (data[, f1] - mean(data[, f1])) * (data[, f2] - mean(data[, f2]))
-  }
-  cov_feature_matrix
-}
-
-squared_from_mean <- function(data) {
-  apply(data, 2, function(x) (x - mean(x))^2)
-}
