8da21f515cf1b255b977b5bdd261811e5c2e1783
Martin Papenberg <martin.papenberg@hhu.de>
2026-02-27 17:31:31 +0100

    also test other interface for opt dispersion

--- inst/tinytest/test-gecode.R
+++ inst/tinytest/test-gecode.R
@@ -20,6 +20,12 @@ if (requireNamespace("gkc.gecode", quietly = TRUE)) {
   Sys.time() - start
   expect_equal(val1, val2)
   
+  start <- Sys.time()
+  groups <- optimal_anticlustering(data, K = K, solver = "gecode", objective = "dispersion")
+  Sys.time() - start
+  val3 <- dispersion_objective(data, groups)
+  expect_equal(val1, val3)
+  
   # Use cannot_link constraints: Element 1 must not be linked with elements 2 to 10:
   cl_matrix <- matrix(c(rep(1, 9), 2:10), ncol = 2)
   cl <- anticlustering(
