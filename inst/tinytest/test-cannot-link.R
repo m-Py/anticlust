e7e3f77c6b427bf513aa52b457ef0ad7cd80f70f
Martin Papenberg <martin.papenberg@hhu.de>
2026-09-15 10:42:09 +0200

    do not source in test files (can't rely on any working directory I guess)

--- inst/tinytest/test-cannot-link.R
+++ inst/tinytest/test-cannot-link.R
@@ -2,7 +2,21 @@
 library(anticlust)
 library(tinytest)
 
-source("./inst/tinytest/gen_data.R")
+# Function to generate data in test files. Used when numeric imprecision is dangerous
+rnd_data_integer <- function(N, M, P = 10, as_factor = FALSE) {
+  if (length(P) == 1) {
+    data <- data.frame(matrix(sample(P, replace = TRUE, size = N*M), ncol = M))
+  } else if (length(P) == M) {
+    data <- data.frame(sapply(P, \(x) sample(x, replace = TRUE, size = N)))
+  } else {
+    stop("Length of P must be 1 or M.")
+  }
+  if (as_factor) {
+    data <- as.data.frame(lapply(data, as.factor))
+  }
+  colnames(data) <- paste0("X", 1:M)
+  data 
+}
 
 # Test if cannot_link constraint works
 a <- anticlustering(rnorm(6), K = 2, cannot_link = cbind(1, 2))
--- inst/tinytest/test-fifr.R
+++ inst/tinytest/test-fifr.R
@@ -1,7 +1,21 @@
 library(anticlust)
 library(tinytest)
 
-source("./inst/tinytest/gen_data.R")
+# Function to generate data in test files. Used when numeric imprecision is dangerous
+rnd_data_integer <- function(N, M, P = 10, as_factor = FALSE) {
+  if (length(P) == 1) {
+    data <- data.frame(matrix(sample(P, replace = TRUE, size = N*M), ncol = M))
+  } else if (length(P) == M) {
+    data <- data.frame(sapply(P, \(x) sample(x, replace = TRUE, size = N)))
+  } else {
+    stop("Length of P must be 1 or M.")
+  }
+  if (as_factor) {
+    data <- as.data.frame(lapply(data, as.factor))
+  }
+  colnames(data) <- paste0("X", 1:M)
+  data 
+}
 
 set.seed(123)
 
